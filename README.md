3c8b79424cad1c748eba02957cb71a0a22651778
Diego Nieto Cid <dnietoc@gmail.com>
2019-04-16 16:31:52 -0300

    Create initial version of plugin

---
+++ README.md
@@ -0,0 +1,74 @@
+# gatsby-transformer-json
+
+Exposes JSON values as a scalar field in GraphQL schema.
+
+## Install
+
+`npm install --save gatsby-transformer-rawjson`
+
+## How to use
+
+In your `gatsby-config.js`:
+
+```javascript
+module.exports = {
+  plugins: [
+    `gatsby-transformer-json`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `./src/data/`,
+      },
+    },
+    `gatsby-transformer-rawjson`,
+  ],
+}
+```
+
+
+## How to query
+
+Assuming a `letters.json` file was loaded
+
+```graphql
+{
+  allLettersJson {
+    edges {
+      node {
+        objectValue {
+          es
+          en
+        }
+        childRawJson {
+          objectValue
+        }
+      }
+    }
+  }
+}
+```
+
+Which would return:
+
+```javascript
+{
+  allLettersJson: {
+    edges: [
+      {
+        node: {
+          objectValue: {
+            "es": "...",
+            "en": "..."
+          }
+          childRawJson {
+            objectValue: {
+              "es": "a",
+              "en": "a"
+            }
+          }
+        },
+      }
+    ]
+  }
+}
+```
---
+++ src/gatsby-node.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const {GraphQLJSON} = require('gatsby/graphql');
+
+exports.onCreateNode = (
+        {node, actions, loadNodeContent, createNodeId, createContentDigest},
+        pluginOptions) => {
+  if (node.internal && node.internal.owner === 'gatsby-transformer-json') {
+    /* It's a JSON node, add child to query scalar */
+    const {
+      id,
+      children,
+      parent,
+      internal,
+      ...rest
+    } = node;
+    const {createNode, createParentChildLink} = actions;
+    const child = {
+      id: createNodeId(`${id} >>> RawJson`),
+      children: [],
+      parent: id,
+      internal: {
+        type: 'RawJson',
+        content: '',
+        contentDigest: createContentDigest('')
+      },
+      ...rest
+    };
+    createNode(child);
+    createParentChildLink({parent: node, child: child});
+  }
+};
+
+exports.setFieldsOnGraphQLNodeType = (args) => {
+  const {type: {name, nodes}} = args;
+  if (name === 'RawJson') {
+    return nodes
+      .map(({id, parent, children, internal, ...rest}) => Object.keys(rest))
+      .reduce((a, f) => a.concat(f), [])
+      .reduce((o, name) => ({...o, [name]: GraphQLJSON}), {});
+  }
+};
